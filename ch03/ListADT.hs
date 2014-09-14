93441ffd56321753c34c4961c89aa864cf06ac33
Rolf Schroder <rolf.schr@gmail.com>
2014-09-14 18:51:54 +0200

    Add toList

---
+++ ch03/ListADT.hs
@@ -0,0 +1,11 @@
+-- file: ch03/ListADT.hs
+data List a = Cons a (List a)
+    | Nil
+    deriving (Show)
+
+fromList (x:xs) = Cons x (fromList xs)
+fromList []     = Nil
+
+toList :: List a -> [a]
+toList Nil = []
+toList (Cons x xs) = x : (toList xs)
