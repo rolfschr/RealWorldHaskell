922e2bba541f844ef2fe58f04fbbe73d445cb3ef
Rolf Schroder <rolf.schr@gmail.com>
2014-09-15 22:16:56 +0200

    Added isPalindrome

--- ch03/palindrome.hs
+++ ch03/palindrome.hs
@@ -3,3 +3,15 @@
 makePalindrome :: [a] -> [a]
 makePalindrome [] = []
 makePalindrome (x:xs) = [x] ++ (makePalindrome xs) ++ [x]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [a] = True
+isPalindrome (x:xs)
+    | x == last xs = isPalindrome (init xs)
+    | otherwise = False
+
+isPalindrome' :: (Eq a) => [a] -> Bool
+isPalindrome' [] = True
+isPalindrome' [a] = True
+isPalindrome' xs = xs == reverse xs
