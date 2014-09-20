9faf6a769154c3e3b9d3805d259d5e720d08689c
Rolf Schroder <rolf.schr@gmail.com>
2014-09-20 10:14:29 +0200

    Added tree height fun

--- ch03/TreeADT.hs
+++ ch03/TreeADT.hs
@@ -1,3 +1,17 @@
 -- file: ch03/TreeADT.hs
 data Tree a = Node a (Maybe (Tree a)) (Maybe (Tree a))
     deriving (Show)
+
+simpleTree = Node "parent" (Just (Node "left child" Nothing Nothing))
+                           (Just (Node "right child" Nothing (Just (Node "rr" Nothing Nothing))))
+
+
+height :: Tree a -> Int
+height (Node a Nothing Nothing) = 1
+height (Node a (Just b) Nothing) = 1 + height b
+height (Node a Nothing (Just b)) = 1 + height b
+height (Node a (Just b) (Just c)) = 1 + max (height b) (height c)
+
+height' :: Maybe (Tree a) -> Int
+height' Nothing = 0
+height' (Just (Node _ l r)) = 1 + max (height' l) (height' r)
