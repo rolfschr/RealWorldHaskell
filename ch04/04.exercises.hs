0303364d0fdb2fb3c7afc10dd92d1723d3e716b1
Rolf Schroder <rolf.schr@gmail.com>
2014-10-09 23:01:23 +0200

    Added takeWhile_fold

--- ch04/04.exercises.hs
+++ ch04/04.exercises.hs
@@ -49,3 +49,30 @@ concat_fold :: [[a]] -> [a]
 concat_fold xs = foldr (++) [] xs
 
 test_concat_fold = concat_fold [[1], [2,3], [4]]
+
+takeWhile_expl :: (a -> Bool) -> [a] -> [a]
+takeWhile_expl p [] = []
+takeWhile_expl p (x:xs)
+    | p x = x : takeWhile_expl p xs
+    | otherwise = []
+
+test_takeWhile_expl = takeWhile_expl (odd) [1,3,5,2,4,7]
+
+-- If we do expression substitution until the end of the list (assuming a finite list), a foldl will end up with the first element in the innermost expression and the last element in the outermost expression. So a strict foldl will start evaluating a foldl with the first element, whereas a lazy foldl will start with the last element.
+--
+-- In the case of foldr, it's the other way round. Therefore a lazy foldr will start with evaluating the expression for the first element, and can handle infinite lists, because it doesn't need to look at any elements after the last one it needs for its result.
+--
+-- Or in another perspective, with an example:
+--
+-- takeWhile_foldr p = foldr f [] where f x xs = if p x then x : xs else []
+--
+-- So this is my take at a solution for this exercise. Important here is the variable xs to the helper function f. This xs is actually a thunk representing the recursive calls foldr does on the following elements of the list. When f returns [], this xs remains unused and will therefore never be forced to evaluate.
+-- foldr f z [x1, x2, ..., xn] == x1 `f` (x2 `f` ... (xn `f` z)...)
+takeWhile_fold :: (a -> Bool) -> [a] -> [a]
+takeWhile_fold p xs = foldr step [] xs
+    where
+        step x ys
+            | p x = x : ys
+            | otherwise = []
+
+test_takeWhile_fold = takeWhile_fold (odd) [1,3,5,2,4,7]
